0699e027b3ed838efb0597f3aad399f8463682f0
fr3m3n <mtimochenko@tutanota.com>
2023-08-12 00:21:46 -0300

    new interface methods

--- app/src/clinic/persistence/IDao.java
+++ app/src/clinic/persistence/IDao.java
@@ -7,4 +7,8 @@ public interface IDao<T> {
     void createTable();
     T insert(T t);
     List<T> selectAll();
+    T selectByID (Long id);
+    T updateByID (Long id);
+    T deleteByID (Long id);
+
 }
