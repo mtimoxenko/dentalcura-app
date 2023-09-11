0f8f73dee5b0e4afba94fc2da99a3c68adba00a6
fr3m3n <mtimochenko@tutanota.com>
2023-09-11 15:05:51 -0300

    security [ToDo]

---
+++ webapp/src/main/java/com/dentalcura/webapp/utils/security/CustomUser.java
@@ -0,0 +1,56 @@
+package com.dentalcura.webapp.utils.security;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomUser implements UserDetails {
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // Implement other UserDetails methods
+}
---
+++ webapp/src/main/java/com/dentalcura/webapp/utils/security/UserRoles.java
@@ -0,0 +1,5 @@
+package com.dentalcura.webapp.utils.security;
+
+public enum UserRoles {
+    USER, ADMIN
+}
